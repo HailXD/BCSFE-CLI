38663dd4daddf4eb4a5301de3311c046429a351f
Hail <jyealc@gmail.com>
2025-08-29 11:58:14 +0800

    IDK what i changed

--- dockerfile
+++ dockerfile
@@ -6,6 +6,9 @@ RUN apt-get update && apt-get install -y python3 python3-pip
 # Install bcsfe
 RUN pip3 install bcsfe
 
+# Create upload directory
+RUN mkdir -p /root/Documents/bcsfe/saves
+
 # Create app directory
 WORKDIR /usr/src/app
 
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
 const pty = require('node-pty');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,8 +12,12 @@ const io = new Server(server);
 
 app.use(express.static(__dirname));
 
+const uploadDir = '/root/Documents/bcsfe/saves';
+
 io.on('connection', (socket) => {
   console.log('a user connected');
+  const sessionDir = path.join(uploadDir, socket.id);
+  fs.mkdirSync(sessionDir, { recursive: true });
 
   const ptyProcess = pty.spawn('python3', ['-m', 'bcsfe'], {
     name: 'xterm-color',
@@ -31,7 +38,28 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected');
     ptyProcess.kill();
+    fs.rm(sessionDir, { recursive: true, force: true }, (err) => {
+      if (err) {
+        console.error(`Error removing session directory: ${err}`);
+      }
+    });
   });
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const sessionDir = path.join(uploadDir, req.query.socketId);
+    cb(null, sessionDir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.send('File uploaded successfully');
 });
 
 server.listen(3000, () => {
